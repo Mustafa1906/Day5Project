27ed1855b6bb2d0d1dadaf37282779ab6ff83516
Mustafa1906 <72093036+Mustafa1906@users.noreply.github.com>
2020-10-13 18:42:39 +0100

    core worker
    This is a conflict

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,6 +3,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Sill awesome hello");
+        System.out.println("Sill awesome hello");//
     }
 }
