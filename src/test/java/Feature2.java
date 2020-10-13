5b71297237bc55fc5085e2fd7cbdc1d288cbacb9
Mustafa1906 <72093036+Mustafa1906@users.noreply.github.com>
2020-10-13 17:56:24 +0100

    feature added  some more

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -4,5 +4,7 @@ public class Feature2 {
         System.out.println("Working on Feature2");
         System.out.println("Working on Feature2 more");
         System.out.println("Again added");
+
+        System.out.println("Adding some more change");
     }
 }
