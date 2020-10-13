b7c301553e865b1e4c53ab8519ad588a50594846
Mustafa1906 <72093036+Mustafa1906@users.noreply.github.com>
2020-10-13 20:19:58 +0100

    new comment added

--- src/test/java/LocalMergeConflict.java
+++ src/test/java/LocalMergeConflict.java
@@ -2,5 +2,6 @@ public class LocalMergeConflict {
 
     public static void main(String[] args) {
         System.out.println("adde some local master brunch");
+        // adding more stuff here
     }
 }
